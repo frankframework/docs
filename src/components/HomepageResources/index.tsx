03822b014b626ed270fbe77d58d47fb8b791bdc3
Sergi Philipsen <philipsen.sergi@gmail.com>
2026-05-07 09:11:11 +0200

    Cange DocCard link to new Manual

--- src/components/HomepageResources/index.tsx
+++ src/components/HomepageResources/index.tsx
@@ -8,7 +8,7 @@ export default function Resources(): ReactNode {
       <div className="container">
         <div className="row">
           <DocCard href='/docs/get-started' title='Get Started' icon='➡️' description='Get up and running with Frank!Framework in minutes.' />
-          <DocCard href='https://frank-manual.readthedocs.io/' title='Manual' icon='📚' description='Documentation about configuration, deployment, testing and operation.' />
+          <DocCard href='/docs/manual' title='Manual' icon='📚' description='Documentation about configuration, deployment, testing and operation.' />
           <DocCard href='https://reference.frankframework.org/' title='Reference' icon='🔣' description='Technical documentation of FF! components.' />
           <DocCard href='https://frankacademy.nl/' title='Frank!Academy' icon='🎓' description='Free training and certification by WeAreFrank!.' />
         </div>
