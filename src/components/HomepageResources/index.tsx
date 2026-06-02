b28b7032374fea8e980669416704a2933cc5d021
Sergi Philipsen <philipsen.sergi@gmail.com>
2026-06-02 13:11:09 +0200

    fix: update DocCard links to include trailing slashes for consistency

--- src/components/HomepageResources/index.tsx
+++ src/components/HomepageResources/index.tsx
@@ -7,8 +7,8 @@ export default function Resources(): ReactNode {
     <section className={styles.resources}>
       <div className="container">
         <div className="row">
-          <DocCard href="/docs/get-started" title="Get Started"icon="➡️" description="Get up and running with Frank!Framework in minutes" />
-          <DocCard href="/docs/manual" title="Manual" icon="📚" description="Documentation about configuration, deployment, testing and operation" />
+          <DocCard href="/docs/get-started/" title="Get Started"icon="➡️" description="Get up and running with Frank!Framework in minutes" />
+          <DocCard href="/docs/manual/" title="Manual" icon="📚" description="Documentation about configuration, deployment, testing and operation" />
           <DocCard href="https://reference.frankframework.org/" title="Reference" icon="🔣" description="Technical documentation of FF! components" />
           <DocCard href="https://insights.frankframework.org/" title="Insights" icon="🔍" description="Insights about implemented features, the roadmap and CVE's" />
           <DocCard href="https://frankacademy.nl/" title="Frank!Academy" icon="🎓" description="Free training and certification by WeAreFrank!" />
